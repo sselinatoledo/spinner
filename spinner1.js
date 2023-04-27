09a8b4dfcc244a286a3849647ae10b64513a2619
sselinatoledo <sselinatoledo@gmail.com>
2023-04-27 22:54:43 +0000

    eslint spinner1.js

--- spinner1.js
+++ spinner1.js
@@ -1,22 +1,22 @@
 setTimeout(() => {
-    process.stdout.write('\r|   ');
-  }, 100);
+  process.stdout.write('\r|   ');
+}, 100);
   
-  setTimeout(() => {
-    process.stdout.write('\r/   ');
-  }, 300);
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 300);
   
-  setTimeout(() => {
-    process.stdout.write('\r-   ');
-  }, 500);
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 500);
   
-  setTimeout(() => {
-    // Need to escape the backslash since it's a special character.
-    process.stdout.write('\r\\ '); 
-  }, 700);
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\ ');
+}, 700);
   
-  setTimeout(() => {
-    process.stdout.write('\r\|  \n'); 
-  }, 900);
+setTimeout(() => {
+  process.stdout.write('\r|  \n');
+}, 900);
   
-  process.stdout.write('hello from spinner1.js... \rheyyy\n');
+process.stdout.write('hello from spinner1.js... \rheyyy\n');
